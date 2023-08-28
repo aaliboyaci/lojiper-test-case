77ec976ec40504d90b06e3234bf0486254a25a08
aaliboyaci <aaliboyaci@gmail.com>
2023-08-28 19:49:51 +0300

    fix mobile responsive scale issues

--- lojiper-test-case/app/layout.tsx
+++ lojiper-test-case/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Lojiper Test Case",
   description: "Lojiper Web Uygulaması Test Case",
+  viewport: "width=device-width, initial-scale=1, maximum-scale=1",
 };
 
 export default function RootLayout({
